a7af91a5116f59389c776b0df41e93bbcd7af277
Meenaketan7 <meenaketanpadhy7@gmail.com>
2025-08-22 14:26:11 +0530

    rename project

--- android/app/src/main/java/com/newapp/me7/MainApplication.kt
+++ android/app/src/main/java/com/newapp/me7/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.salus
+package com.newapp.me7
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/NewAppME7/AppDelegate.swift
+++ ios/NewAppME7/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "Salus",
+      withModuleName: "New App ME7",
       in: window,
       launchOptions: launchOptions
     )
